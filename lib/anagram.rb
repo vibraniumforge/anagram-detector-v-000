425d4e3ec6453035a0d3434a0789326625d5df92
Greg Morgan <gregm9999@gmail.com>
2018-11-10 02:23:27 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,14 @@
 # Your code goes here!
 class Anagram
-attr_accessor :words
+attr_accessor :word
 
-  def initialize(words)
-    @words=words
+  def initialize(word)
+    @word=word
   end
 
-  def match(words)
-    if 
+  def match(word, possible_anagrams)
+    answer=[]
+    
   end
 
 end
